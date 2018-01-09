8de92a2fc94ed1c49e9240119c57ed951c299770
Rajat Garg <rg51096@gmail.com>
2018-01-09 12:47:08 +0530

    training nd testing

--- linear-regr-train-test.py
+++ linear-regr-train-test.py
@@ -2,15 +2,15 @@ import pandas as pd
 import quandl, math
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
-
+from sklearn.linear_model import LinearRegression
 df = quandl.get("WIKI/GOOGL")
-#print(df.head())
+
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close']*100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open']*100.0
 
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
-#print(df.head())
+
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
