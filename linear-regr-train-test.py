39b91bb4ccebafc931ac34a8c03ae04d53f94c22
Rajat Garg <rg51096@gmail.com>
2018-01-09 13:08:03 +0530

    accuracy with svm

--- linear-regr-train-test.py
+++ linear-regr-train-test.py
@@ -32,7 +32,7 @@ y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = LinearRegression()
+clf = svm.SVR()
 clf.fit(X_train, y_train) #train
 accuracy = clf.score(X_test, y_test) #test
 
